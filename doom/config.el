b4319538f7fc02da664d5070f12d14c97b4b38c9
Kevin Tran <91237886+Kevonosdiaz@users.noreply.github.com>
2023-11-16 09:49:33 -0700

    add projectile project path to repos folder and vertico icons

--- doom/config.el
+++ doom/config.el
@@ -23,6 +23,8 @@
 ;;
 (setq doom-font (font-spec :family "JetBrainsMono NF" :size 15)
       doom-variable-pitch-font (font-spec :family "Monospace" :size 15))
+
+(setq projectile-project-search-path '("~/repos/"))
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
@@ -41,7 +43,6 @@
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
-
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
