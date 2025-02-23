9b0d20491f9ba20be17791242edf5c3ca8fdcf48
Kevin Tran <91237886+Kevonosdiaz@users.noreply.github.com>
2025-02-23 16:47:59 -0700

    add more stuff to post install script

--- arch-postinstall.sh
+++ arch-postinstall.sh
@@ -3,7 +3,7 @@ sudo rmmod pcspkr
 sudo rmmod snd_pcsp
 
 # Install stuff and services
-sudo pacman -S --noconfirm --needed bluez bluez-utils git pipewire easyeffects vlc lsp-plugins amd-ucode noto-fonts noto-fonts-cjk timeshift fastfetch flameshot sddm pacman-contrib
+sudo pacman -S --noconfirm --needed bluez bluez-utils git pipewire easyeffects vlc lsp-plugins amd-ucode noto-fonts noto-fonts-cjk noto-fonts-emoji timeshift fastfetch flameshot sddm pacman-contrib zoxide ranger fzf ripgrep python3 alacritty kitty btop bat ncdu vim neovim starship tmux unzip
 sudo systemctl enable --now bluetooth.service
 sudo systemctl enable sddm.service
 sudo systemctl enable NetworkManager.service
@@ -14,5 +14,7 @@ sudo systemctl enable fstrim.timer
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
+
+yay -S --noconfirm --needed spicetify-cli spicetify-marketplace-bin moc-pulse
 
 
