96498823d4e0a9330cc1a85ba07253e7326d22ee
Kevin Tran <91237886+Kevonosdiaz@users.noreply.github.com>
2025-03-04 19:15:54 -0700

    include fuzzel, add nm-applet start and touchpad scroll

--- install-sway.sh
+++ install-sway.sh
@@ -2,5 +2,5 @@
 sudo pacman -S --noconfirm --needed sway brightnessctl libpulse mako polkit swaybg sway-contrib swayidle swaylock waybar wmenu xorg-xwayland xdg-desktop-portal-gtk xdg-desktop-portal-wlr fuzzel kitty dolphin
 
 # Copy dots
-cp -r waybar sway ~/.config/
+cp -r waybar sway fuzzel ~/.config/
 ./reload-dots.sh
