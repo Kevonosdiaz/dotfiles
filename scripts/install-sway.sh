d993cab27704fb5e23cd3a2eb9111be9dca11720
Kevin Tran <kevonosdiaz@gmail.com>
2025-03-10 19:10:04 -0600

    add rofi to install script

--- scripts/install-sway.sh
+++ scripts/install-sway.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo pacman -S --noconfirm --needed sway brightnessctl libpulse mako polkit swaybg sway-contrib swayidle swaylock waybar wmenu xorg-xwayland xdg-desktop-portal-gtk xdg-desktop-portal-wlr fuzzel kitty dolphin gnome-keyring qt6ct
+sudo pacman -S --noconfirm --needed sway brightnessctl libpulse mako polkit swaybg sway-contrib swayidle swaylock waybar wmenu xorg-xwayland xdg-desktop-portal-gtk xdg-desktop-portal-wlr fuzzel kitty dolphin gnome-keyring qt6ct inetutils
 
 # Copy dots
-cd ..; cp -r waybar sway fuzzel qt6ct/colors ~/.config/
+cd ..; cp -r waybar sway fuzzel qt6ct/colors rofi ~/.config/
 # ./reload-dots.sh
