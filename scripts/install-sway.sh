4c6b1029ed7ac5567f13f13d18ad54ba0f51c848
Kevin Tran <kevonosdiaz@gmail.com>
2025-03-29 13:46:38 -0600

    use pavucontrol-qt in sway/waybar, modularize input and window rules initially

--- scripts/install-sway.sh
+++ scripts/install-sway.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo pacman -S --noconfirm --needed sway brightnessctl libpulse mako polkit swaybg sway-contrib swayidle swaylock waybar wmenu xorg-xwayland xdg-desktop-portal-gtk xdg-desktop-portal-wlr fuzzel kitty dolphin gnome-keyring qt6ct inetutils
+sudo pacman -S --noconfirm --needed sway brightnessctl libpulse mako polkit swaybg sway-contrib swayidle swaylock waybar wmenu xorg-xwayland xdg-desktop-portal-gtk xdg-desktop-portal-wlr fuzzel kitty dolphin gnome-keyring qt6ct inetutils autotiling
 
 # Copy dots
 cd ..; cp -r waybar sway fuzzel qt6ct/colors rofi ~/.config/
