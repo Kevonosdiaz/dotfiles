9767ded5765a9edd457d11549ee1bbabafe9faa2
Kevin Tran <kevonosdiaz@gmail.com>
2025-06-11 20:00:21 -0600

    wezterm always sources bashrc, font 13 + use wezterm in sway

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
@@ -11,55 +11,56 @@ config.initial_cols = 120
 config.initial_rows = 28
 
 -- or, changing the font size and color scheme.
-config.font = wezterm.font 'JetBrainsMono Nerd Font'
-config.font_size = 14
-config.color_scheme = 'Catppuccin Mocha'
-config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' } -- Disable font ligatures
+config.font = wezterm.font("JetBrainsMono Nerd Font")
+config.font_size = 13
+config.color_scheme = "Catppuccin Mocha"
+config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" } -- Disable font ligatures
 
 config.enable_scroll_bar = true
+config.default_prog = { "bash" }
 
 -- Keymapping stuff
-config.leader = { key = 's', mods = 'CTRL', timeout_milliseconds = 1000 }
+config.leader = { key = "s", mods = "CTRL", timeout_milliseconds = 1000 }
 config.keys = {
-    -- splitting
-    {
-        mods   = "LEADER",
-        key    = "w",
-        action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' }
-    },
-    {
-        mods   = "LEADER",
-        key    = "v",
-        action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' }
-    },
+	-- splitting
+	{
+		mods = "LEADER",
+		key = "w",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		mods = "LEADER",
+		key = "v",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
 
-    -- window navigation
-    {
-        key = "h",
-        mods = "ALT",
-        action = wezterm.action_callback(function(window, pane)
-            local tab = window:mux_window():active_tab()
-            if tab:get_pane_direction("Left") ~= nil then
-                window:perform_action(wezterm.action.ActivatePaneDirection("Left"), pane)
-            else
-                window:perform_action(wezterm.action.ActivateTabRelative(-1), pane)
-            end
-        end),
-    },
-    { key = "j", mods = "ALT", action = wezterm.action.ActivatePaneDirection("Down") },
-    { key = "k", mods = "ALT", action = wezterm.action.ActivatePaneDirection("Up") },
-    {
-        key = "l",
-        mods = "ALT",
-        action = wezterm.action_callback(function(window, pane)
-            local tab = window:mux_window():active_tab()
-            if tab:get_pane_direction("Right") ~= nil then
-                window:perform_action(wezterm.action.ActivatePaneDirection("Right"), pane)
-            else
-                window:perform_action(wezterm.action.ActivateTabRelative(1), pane)
-            end
-        end)
-    }
+	-- window navigation
+	{
+		key = "h",
+		mods = "ALT",
+		action = wezterm.action_callback(function(window, pane)
+			local tab = window:mux_window():active_tab()
+			if tab:get_pane_direction("Left") ~= nil then
+				window:perform_action(wezterm.action.ActivatePaneDirection("Left"), pane)
+			else
+				window:perform_action(wezterm.action.ActivateTabRelative(-1), pane)
+			end
+		end),
+	},
+	{ key = "j", mods = "ALT", action = wezterm.action.ActivatePaneDirection("Down") },
+	{ key = "k", mods = "ALT", action = wezterm.action.ActivatePaneDirection("Up") },
+	{
+		key = "l",
+		mods = "ALT",
+		action = wezterm.action_callback(function(window, pane)
+			local tab = window:mux_window():active_tab()
+			if tab:get_pane_direction("Right") ~= nil then
+				window:perform_action(wezterm.action.ActivatePaneDirection("Right"), pane)
+			else
+				window:perform_action(wezterm.action.ActivateTabRelative(1), pane)
+			end
+		end),
+	},
 }
 
 -- Finally, return the configuration to wezterm:
