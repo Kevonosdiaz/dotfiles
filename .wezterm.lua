e2eb54ccf3961e99c8e4dce80dbe44dd61659636
Kevin Tran <kevonosdiaz@gmail.com>
2025-06-11 20:24:27 -0600

    yoinked an example tabbar for wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -19,6 +19,36 @@ config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" } -- Disable font liga
 config.enable_scroll_bar = true
 config.default_prog = { "bash" }
 
+-- Customize top tabbar
+config.window_frame = {
+	font = wezterm.font({ family = "JetBrainsMono Nerd Font" }),
+	font_size = 12,
+}
+
+wezterm.on("update-status", function(window)
+	-- Grab the utf8 character for the "powerline" left facing
+	-- solid arrow.
+	local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+
+	-- Grab the current window's configuration, and from it the
+	-- palette (this is the combination of your chosen colour scheme
+	-- including any overrides).
+	local color_scheme = window:effective_config().resolved_palette
+	local bg = color_scheme.background
+	local fg = color_scheme.foreground
+
+	window:set_right_status(wezterm.format({
+		-- First, we draw the arrow...
+		{ Background = { Color = "none" } },
+		{ Foreground = { Color = bg } },
+		{ Text = SOLID_LEFT_ARROW },
+		-- Then we draw our text
+		{ Background = { Color = bg } },
+		{ Foreground = { Color = fg } },
+		{ Text = " " .. wezterm.hostname() .. " " },
+	}))
+end)
+
 -- Keymapping stuff
 config.leader = { key = "s", mods = "CTRL", timeout_milliseconds = 1000 }
 config.keys = {
@@ -60,6 +90,16 @@ config.keys = {
 				window:perform_action(wezterm.action.ActivateTabRelative(1), pane)
 			end
 		end),
+	},
+
+	-- shortcuts
+	{
+		key = ",",
+		mods = "SUPER",
+		action = wezterm.action.SpawnCommandInNewTab({
+			cwd = wezterm.home_dir,
+			args = { "nvim", wezterm.config_file },
+		}),
 	},
 }
 
